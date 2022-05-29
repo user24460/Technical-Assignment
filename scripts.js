d63557cfbc7524f9b0eb1b2ccb3e83b59ce09840
Celson Gutkowski <idkdavidc@gmail.com>
2022-05-28 20:00:11 -0400

    First commit

---
+++ scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  $('#star1').on("click", function() {
+      $('#star1').css({ fill: "#ffea03" });
+  });
+  $('#star2').on("click", function() {
+      $('#star2').css({ fill: "#ffea03" });
+  });
+  $('#star3').on("click", function() {
+      $('#star3').css({ fill: "#ffea03" });
+  });
+  $('#star4').on("click", function() {
+      $('#star4').css({ fill: "#ffea03" });
+  });
+  $('#star5').on("click", function() {
+      $('#star5').css({ fill: "#ffea03" });
+  });
+  $('#star6').on("click", function() {
+      $('#star6').css({ fill: "#ffea03" });
+  });
+  $('#star7').on("click", function() {
+      $('#star7').css({ fill: "#ffea03" });
+  });
+  $('#star8').on("click", function() {
+      $('#star8').css({ fill: "#ffea03" });
+  });
+});
